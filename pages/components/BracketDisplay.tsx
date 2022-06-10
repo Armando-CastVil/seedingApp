fdbeefbeecaed9f2be9c9f793c4c26fe01cefb74
Armando Castaneda <armando.castvil@gmail.com>
2022-06-09 17:14:04 -0700

    initialized setup for bracket displayer

---
+++ pages/components/BracketDisplay.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SingleEliminationBracket, DoubleEliminationBracket, Match, SVGViewer } from '@g-loot/react-tournament-brackets';
+import { dummymatches } from "./dummymatches";
+export default function BracketDisplay()
+{
+    return(
+    
+    <DoubleEliminationBracket
+        matches={dummymatches}
+        matchComponent={Match}
+     
+    />)
+
+}
+
