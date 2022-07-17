1625c96b6085991abf766dd95a60043e5d6a6039
Stephen-Schuster <46509946+Stephen-Schuster@users.noreply.github.com>
2022-07-17 16:10:03 -0700

    Removed Seed and loadTournament
    failed refactor being removed

--- pages/modules/Seed.ts
+++
@@ -1,9 +0,0 @@
-export default class Seed
-{
-    projectedPath: Seed[] | null;
-	seedNum:number;
-    constructor(projectedPath:Seed[]|null, seedNum:number) {
-        this.seedNum = seedNum;
-        this.projectedPath = projectedPath;
-    }
-}
