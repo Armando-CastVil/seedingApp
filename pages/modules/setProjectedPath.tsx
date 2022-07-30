e5afd974d54e8e6b5098c3a17404a5faa8af52f6
Armando Castaneda <armando.castvil@gmail.com>
2022-07-30 15:26:38 -0700

    fix function to add competitor object instead of participant

--- pages/modules/setProjectedPath.tsx
+++ pages/modules/setProjectedPath.tsx
@@ -9,19 +9,20 @@ interface MatchStructure
 export default function setProjectedPath(matchList:MatchStructure,playerList:Competitor[],bracketIDs:number[])
 {
 
+
     //set players from projected path in Winners
     for(let i=0;i<matchList.upper.length;i++)
     {
-        playerList[bracketIDs.indexOf(matchList.upper[i].participants[0].bracketID as unknown as number)].addPlayerToPath(matchList.upper[i].participants[1])
-        playerList[bracketIDs.indexOf(matchList.upper[i].participants[1].bracketID as unknown as number)].addPlayerToPath(matchList.upper[i].participants[0])
+        playerList[bracketIDs.indexOf(matchList.upper[i].participants[0].bracketID as unknown as number)].addPlayerToPath( playerList[bracketIDs.indexOf(matchList.upper[i].participants[1].bracketID as unknown as number)])
+        playerList[bracketIDs.indexOf(matchList.upper[i].participants[1].bracketID as unknown as number)].addPlayerToPath( playerList[bracketIDs.indexOf(matchList.upper[i].participants[0].bracketID as unknown as number)])
         
     }
 
      //set players from projected path in Winners
      for(let i=0;i<matchList.lower.length;i++)
      {
-         playerList[bracketIDs.indexOf(matchList.lower[i].participants[0].bracketID as number)].addPlayerToPath(matchList.lower[i].participants[1])
-         playerList[bracketIDs.indexOf(matchList.lower[i].participants[1].bracketID as number)].addPlayerToPath(matchList.lower[i].participants[0])
+         playerList[bracketIDs.indexOf(matchList.lower[i].participants[0].bracketID as number)].addPlayerToPath( playerList[bracketIDs.indexOf(matchList.upper[i].participants[1].bracketID as unknown as number)])
+         playerList[bracketIDs.indexOf(matchList.lower[i].participants[1].bracketID as number)].addPlayerToPath( playerList[bracketIDs.indexOf(matchList.upper[i].participants[0].bracketID as unknown as number)])
          
      }
 
